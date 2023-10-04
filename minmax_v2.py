093aa2337b61ecbab868ed045752274721fd8572
rayanejaffal <130594483+rayanejaffal@users.noreply.github.com>
2023-10-05 01:55:46 +0200

    Update minmax_v2.py

--- minmax_v2.py
+++ minmax_v2.py
@@ -1,1 +1,17 @@
+#!/bin/env python3
+import sys
+from acquisition_methods import read_list
+def minmax(l):
 
+    if type(l[0])==int:
+        maxi.append(max(l))
+    else:
+        for i in l:
+            minmax(i)
+
+if __name__=="__main__":
+    # programme principal
+    l = read_list()                   # rÃ©cupÃ©ration de la liste
+    maxi = []
+    minmax(l)
+    print(min(maxi))
